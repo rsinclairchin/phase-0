cbf98829a1416695a6fd82033dcf782776a67ab9
r.sinclairchin <r.sinclairchin@gmail.com>
2015-09-13 10:48:52 -0700

    add add-it-up

---
+++ week-4/add-it-up/my_solution.rb
@@ -0,0 +1,47 @@
+# Add it up!
+
+# Complete each step below according to the challenge directions and
+# include it in this file. Also make sure everything that isn't code
+# is commented in the file.
+
+# I worked on this challenge [with: Eric Shou].
+
+# 0. total Pseudocode
+# make sure all pseudocode is commented out!
+
+# Input: Array of integer
+# Output: Sum of integers in the array
+# Steps to solve the problem.
+
+
+# 1. total initial solution
+
+def total(num)
+  sum = 0
+  num.each do |int|
+  sum = sum + int
+  end
+  return sum
+end
+
+def sentence_maker(str)
+str[0].capitalize!
+sentence = str.join(' ') +'.'
+end
+
+# 3. total refactored solution
+
+
+
+# 4. sentence_maker pseudocode
+# make sure all pseudocode is commented out!
+# Input:
+# Output:
+# Steps to solve the problem.
+
+
+# 5. sentence_maker initial solution
+
+
+
+# 6. sentence_maker refactored solution
