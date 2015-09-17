7004931c3c64bf26034f25fc91fb433d7b12e4d9
r.sinclairchin <r.sinclairchin@gmail.com>
2015-09-17 10:28:05 -0700

    add refactored code to acct_groups

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -5,6 +5,7 @@
 #   take two people out of two different groups of 5
 #   put them into the last group
 
+# Initial Solution
 def group(peeps)
   groups = []
   peeps.each_slice(5) {|a| groups << a}
@@ -15,3 +16,25 @@ def group(peeps)
   end
   p groups
 end
+
+# Refactored Solution
+def group(peeps)
+  groups = []
+  peeps.each_slice(5) {|a| groups << a}
+  groups[-2].pop
+  i=-1
+  while i > -5
+    until groups[i].length >= 4
+      groups[i] << groups[i-1].pop
+    end
+    i -= 1
+  end
+  p groups
+end
+
+# Fancied Up Solution
+
+
+# Test
+peeps = ["Shunqian Luo (Nathan)", "Kim Allen","Jenna Andersen", "Pamela Antonow", "Jupiter Baudot", "Ovi Calvo", "Awilda Cantres", "Celeste Carter", "Mike Cerrone", "Eunice Choi","Un Choi", "Kristie Chow", "Regina Compton", "Marcus Davis", "Paul Etscheit", "Solomon Fernandez", "Marcel Galang", "Ricky Binhai Hu", "Kai Huang", "Harmin Jeong", "Lauren Jin", "CJ Joulain", "Michael Landon", "Richard Leo", "Julian Lesse", "Ryan Lesson", "Ian Lockwood", "Christopher Mark", "Lauren Markzon", "Miguel Angel Melendez", "David O'Keefe", "Nathan Park", "Farman Pirzada", "Samuel Frederick Purcell", "Salim Rahimi", "Colin Razevich", "Irina Renteria", "Jamie Runyan", "Eric Shou", "Rosslyn Sinclair-Chin", "Aji Slater", "Marvy Tagala", "George Warnbold", "Shawn Watson", "Hana Worku", "Nicholas Yee"]
+group(peeps)
